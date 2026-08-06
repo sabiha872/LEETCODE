c0e9829cea0e6d16d8bee106cdac0e3e7a2f4d31
Sabiha Khan <sabiha.23bce11638@vitbhopal.ac.in>
2026-08-06 18:20:49 +0530

    Time: 68 ms (13.30%) | Memory: 121.1 MB (23.20%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        unordered_map<int, int> degree;
+        for(auto &vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+
+            degree[u]++;
+            degree[v]++;
+        }
+        for(auto &it: degree) {
+            if(it.second == edges.size()) {
+                return it.first;     
+            }
+        }
+            return -1;    
+    }
+};
