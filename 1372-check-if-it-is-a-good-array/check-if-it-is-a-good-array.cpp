d4f05683d60a88ea789eb9fbde245bcc920b3daa
Sabiha Khan <sabiha.23bce11638@vitbhopal.ac.in>
2026-08-13 00:44:51 +0530

    Time: 3 ms (50.40%) | Memory: 33.1 MB (22.72%) - LeetSync

---
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+
+        int g = 0;
+
+        for(int num : nums) {
+            g = gcd(g, num);
+        }
+
+        return g == 1;
+    }
+};
