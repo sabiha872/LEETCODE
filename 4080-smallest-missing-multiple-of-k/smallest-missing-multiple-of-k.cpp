0d7262dd7e915ac7ce76b9c0d51f776af03f7fd6
Sabiha Khan <sabiha.23bce11638@vitbhopal.ac.in>
2026-08-26 04:42:51 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (82.02%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        int multiple = k;
+        while(true) {
+            bool found = false;
+
+            for(int x : nums) {
+                if(x == multiple) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found == false)
+               return multiple;
+            
+           multiple = multiple + k;
+        }
+        
+    }
+};
