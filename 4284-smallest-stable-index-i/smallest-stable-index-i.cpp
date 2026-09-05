ea67fa23901ec64cbef5367435a01789ec1e9b24
Sabiha Khan <sabiha.23bce11638@vitbhopal.ac.in>
2026-09-06 00:46:53 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (76.38%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> rightMin(n);
 
-        rightMin[n - 1] = nums[n - 1] ;
+        for (int i = 0; i < n; i++) {
 
-        for (int i = n - 2; i >=0 ; i--)  {
-            rightMin[i] = min(nums[i], rightMin[i+ 1]);
-        }
-        int leftMax = nums[0];
-        for ( int i = 0; i < n; i++) {
-            leftMax = max(leftMax, nums[i]);
+            int maxLeft = nums[0];
 
-            int instability = leftMax - rightMin[i];
-            if (instability <=k) {
+            for (int j = 0; j <= i; j++) {
+                maxLeft = max(maxLeft, nums[j]);
+            }
+
+            int minRight = nums[i];
+
+            for (int j = i; j < n; j++) {
+                minRight = min(minRight, nums[j]);
+            }
+
+            int instability = maxLeft - minRight;
+
+            if (instability <= k) {
                 return i;
             }
         }
+
         return -1;
-        
     }
 };
